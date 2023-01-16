aa63f5fa02040774760bcdbc44bf08140c7c5176
Kushnir Maxim <maxbasta2006@gmail.com>
2023-01-16 18:59:38 +0100

    database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,15 @@ import { AppController } from './app.controller';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { AuthGuard } from '@nestjs/passport/dist';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { typeOrmConfig } from './config/typeorm.config';
 
 @Module({
-	imports: [AuthModule, UsersModule],
+	imports: [
+		AuthModule, 
+		UsersModule,
+		TypeOrmModule.forRoot(typeOrmConfig)
+	],
 	controllers: [AppController],
 	providers: [],
 })
